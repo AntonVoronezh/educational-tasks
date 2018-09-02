9420aec678efeb38f8713357d9e66edc790a4c4d
AntonVoronezh <pstut@mail.ru>
2018-09-02 18:50:40 +0300

    task 10

--- 10/task.js
+++ 10/task.js
@@ -1,11 +1,10 @@
 const sumSquareDifference = (n) => {
   let kvSum = 0;
-  let sumKv = 0; 
-    for (let i = 1; i <= n; i += 1) {
-      sumKv += Math.pow(i, 2);
-      kvSum += i; 
-      //console.log(sumKv);
-    }
-  return(Math.pow(kvSum, 2) - sumKv); 
-}
+  let sumKv = 0;
+  for (let i = 1; i <= n; i += 1) {
+    sumKv += Math.pow(i, 2);
+    kvSum += i;
+  }
+  return (Math.pow(kvSum, 2) - sumKv);
+};
 export default sumSquareDifference;
