90d34769c41ac7ece4cd8c63ae546bb8f6e136db
AntonVoronezh <pstut@mail.ru>
2018-09-04 11:52:37 +0300

    11 #5

--- 11/task.js
+++ 11/task.js
@@ -1,18 +1,12 @@
 const isPerfect = (n) => {
   let acc = 0;
-
-
-
-  for (let i = 0; i <= n-1; i += 1) {
-    if(n % i === 0){
+  for (let i = 0; i <= n - 1; i += 1) {
+    if (n % i === 0) {
       acc += i;
-      
-      console.log(i);
     }
-  } 
+  }
   if (n !== acc || n === 0) {
     return false;
-  } else {
-    return true;
-  } 
-}
+  }
+  return true;
+};
