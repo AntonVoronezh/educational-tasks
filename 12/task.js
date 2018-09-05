915cdafcc6a07a3549fddc2cc296ab080ec120a3
AntonVoronezh <pstut@mail.ru>
2018-09-05 19:01:02 +0300

    12 #6

--- 12/task.js
+++ 12/task.js
@@ -1,6 +1,5 @@
 import { length, substr } from './strings';
 
-// BEGIN (write your solution here)
 const sum = (str) => {
   let result = 0;
   for (let i = 0; i < length(str); i += 1) {
@@ -10,18 +9,13 @@ const sum = (str) => {
 };
 
 const isHappyTicket = (n) => {
-  if (typeof(n) === 'number'){n = String(n);}
-      //console.log(typeof(n));
-  let subLevo = substr(n, 0, 3);
-  let subPravo = substr(n, 3, 3);
-  //console.log(subLevo);
-  //console.log(subPravo);
+  if (typeof (n) === 'number') { n = String(n); }
+  const subLevo = substr(n, 0, 3);
+  const subPravo = substr(n, 3, 3);
   if (sum(subLevo) === sum(subPravo)) {
     return true;
-  } else {
-    return false;
   }
-}
+  return false;
+};
 
 export default isHappyTicket;
-// END
